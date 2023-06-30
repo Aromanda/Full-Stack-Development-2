fcda5cbe95706dcd0cb912298a8a2b77abcba10a
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-06-29 21:36:56 -0400

    create error page

---
+++ client/src/components/error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Error() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1 style={{ color: 'red' }}>Unauthorized</h1>
+      <p style={{ fontStyle: 'italic' }}>You are not authorized to access this page.</p>
+    </div>
+  );
+};
+
+
