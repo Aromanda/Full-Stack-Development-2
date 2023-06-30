5a88c7b41ad3dfb32a5ca2e71513ac52ea5e10aa
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-06-29 21:36:02 -0400

    add error and place recordList  As main page

--- client/src/App.js
+++ client/src/App.js
@@ -9,16 +9,18 @@ import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
 import Login from "./components/login";
+import Error from "./components/error";
 
 const App = () => {
  return (
    <div>
      <Navbar />
      <Routes>
-       <Route exact path="/" element={<RecordList />} />
+       <Route exact path="/recordList" element={<RecordList />} />
        <Route path="/edit/:id" element={<Edit />} />
-       <Route path="/login" element={<Login />} />
+       <Route path="/" element={<Login />} />
        <Route path="/create" element={<Create />} />
+       <Route path="/error" element={<Error />} />
      </Routes>
    </div>
  );
